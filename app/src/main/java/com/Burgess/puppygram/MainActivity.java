86861371c9addc66d640491df6048d23a933c4a4
noahkrill11 <nlk34@zips.uakron.edu>
2021-03-10 23:11:46 -0500

    Add button functionality

--- app/src/main/java/com/Burgess/puppygram/MainActivity.java
+++ app/src/main/java/com/Burgess/puppygram/MainActivity.java
@@ -2,13 +2,30 @@ package com.Burgess.puppygram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button CreateNewButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CreateNewButton =(Button) findViewById(R.id.newAccount);
+        CreateNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewAccount();
+            }
+        });
+
+    }
+    public void openNewAccount()
+    {
+        Intent intent = new Intent(this, NewAccount.class);
+        startActivity(intent);
+
     }
 }
