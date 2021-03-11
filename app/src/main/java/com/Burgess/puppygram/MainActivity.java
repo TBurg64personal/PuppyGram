4ea2bc1e2dda1faddec9b8a35a93702dca830af0
noahkrill11 <nlk34@zips.uakron.edu>
2021-03-11 17:55:00 -0500

    Add button connectivity for the name

--- app/src/main/java/com/Burgess/puppygram/MainActivity.java
+++ app/src/main/java/com/Burgess/puppygram/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button CreateNewButton;
+    private Button NewAccount;
+    private EditText fName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,14 @@ public class MainActivity extends AppCompatActivity {
                 openNewAccount();
             }
         });
-
+        NewAccount = (Button) findViewById(R.id.Submit);
+        NewAccount.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                fName = (EditText)findViewById(R.id.firstNameInput);
+            }
+        });
     }
     public void openNewAccount()
     {
