7d162d71e63a8add843d18d5c1663f1f3b44bfaf
noahkrill11 <nlk34@zips.uakron.edu>
2021-03-11 20:02:58 -0500

    Add connection to database

--- app/src/main/java/com/Burgess/puppygram/NewAccount.java
+++ app/src/main/java/com/Burgess/puppygram/NewAccount.java
@@ -2,10 +2,19 @@ package com.Burgess.puppygram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.SymbolTable;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class NewAccount extends AppCompatActivity {
     private Button NewAccount;
@@ -13,6 +22,8 @@ public class NewAccount extends AppCompatActivity {
     private EditText lNameID;
     private String fname;
     private String lname;
+    public Connection conn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +40,32 @@ public class NewAccount extends AppCompatActivity {
     }
     public void createNewAccount()
     {
+        conn = connectionClass();
+
         fNameID = (EditText)findViewById(R.id.firstNameInput);
         lNameID = (EditText)findViewById(R.id.lastNameInput);
         fname =fNameID.getText().toString();
         lname = lNameID.getText().toString();
 
+
+    }
+    public Connection connectionClass()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://puppygram.database.windows.net:1433;DatabaseName=Puppygram;user=Noahkrill@puppygram;password=Uakron2019;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(ConnectionURL);
+
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
     }
 }
