a2af5e6244f6da74751a1152e9af53a48adeefc3
noahkrill11 <nlk34@zips.uakron.edu>
2021-03-15 19:23:28 -0400

    Add new button for log in

--- app/src/main/java/com/Burgess/puppygram/Login.java
+++ app/src/main/java/com/Burgess/puppygram/Login.java
@@ -9,23 +9,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        loginButton = (Button) findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                CheckAccount();
             }
         });
+    }
+    public void CheckAccount()
+    {
+
     }
 }
