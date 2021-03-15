1cb1a17f9e12d7f04719a792e6920ab300c9a9ea
noahkrill11 <nlk34@zips.uakron.edu>
2021-03-15 19:15:04 -0400

    Add login page

--- app/src/main/java/com/Burgess/puppygram/MainActivity.java
+++ app/src/main/java/com/Burgess/puppygram/MainActivity.java
@@ -11,16 +11,24 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private Button CreateNewButton;
+    private Button LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CreateNewButton =(Button) findViewById(R.id.newAccount);
+        LoginButton = (Button) findViewById(R.id.logIn);
         CreateNewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openNewAccount();
+            }
+        });
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logintonew();
             }
         });
 
@@ -30,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewAccount.class);
         startActivity(intent);
 
+    }
+    public void logintonew()
+    {
+        Intent intent = new Intent(this, Login.class );
+        startActivity(intent);
     }
 
 }
