f95f578bc1572d3292278b1e65f529eb4953accf
clt94 <clt94@zips.uakron.edu>
2021-03-19 21:21:28 -0400

    add upload new picture page

---
+++ app/src/main/java/com/Burgess/puppygram/NewPicture.java
@@ -0,0 +1,14 @@
+package com.Burgess.puppygram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_picture);
+    }
+}
