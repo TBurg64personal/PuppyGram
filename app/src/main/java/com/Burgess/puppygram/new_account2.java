1cb8fe015979458ebdf1f044f72834a4b4bfe332
TBurg64 <70659543+TBurg64@users.noreply.github.com>
2021-03-21 16:57:04 -0400

    finish the second login page asking about animal information

---
+++ app/src/main/java/com/Burgess/puppygram/new_account2.java
@@ -0,0 +1,14 @@
+package com.Burgess.puppygram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_account2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account2);
+    }
+}
