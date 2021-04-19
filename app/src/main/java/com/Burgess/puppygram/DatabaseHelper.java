76a443866ab0dbf9d2a49b25a436fb472cb98cf9
noahkrill11 <nlk34@zips.uakron.edu>
2021-04-19 16:42:32 -0400

    Change db

---
+++ app/src/main/java/com/Burgess/puppygram/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.Burgess.puppygram;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.nfc.Tag;
+import android.util.Log;
+import android.widget.TableRow;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String TAG = "DatabaseHelper";
+    private static final String TABLE_NAME = "Accounts";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "FirstName";
+    private static final String COL3 = "LastName";
+    private static final String COL4 = "Email";
+    private static final String COL5 = "Password";
+    private static final String COL6 = "UserName";
+
+    public DatabaseHelper(@Nullable Context context) {
+    super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " +TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                COL2 + " TEXT NOT NULL, " + COL3 + " TEXT NOT NULL, "+COL4+" TEXT NOT NULL, "+COL5+" TEXT NOT NULL, "+COL6+" TEXT NOT NULL )";
+        db.execSQL(createTable);
+
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int i, int i1)
+    {
+        String var = "DROP IF TABLE EXISTS ";
+        db.execSQL(var+ TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String firstName, String lastName, String Email, String Password, String UserName)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentvals = new ContentValues();
+        contentvals.put(COL2, firstName);
+        contentvals.put(COL3,lastName);
+        contentvals.put(COL4,Email);
+        contentvals.put(COL5,Password);
+        contentvals.put(COL6,UserName);
+
+        Log.d(TAG, "Adding Data ");
+        long result= db.insert(TABLE_NAME, null, contentvals);
+
+        if(result ==-1)
+            return  false;
+        else
+            return true;
+
+
+    }
+
+}
--- app/src/main/java/com/Burgess/puppygram/NewAccount.java
+++ app/src/main/java/com/Burgess/puppygram/NewAccount.java
@@ -3,17 +3,16 @@ package com.Burgess.puppygram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.icu.text.SymbolTable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 
@@ -35,13 +34,15 @@ public class NewAccount extends AppCompatActivity {
     private Statement stmt = null;
     private boolean isAccount = false;
     private boolean isUserName = false;
+    DatabaseHelper DatabaseHelp1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
         NewAccount = (Button) findViewById(R.id.Submit);
-
+        DatabaseHelp1 = new DatabaseHelper(this);
         NewAccount.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -50,9 +51,7 @@ public class NewAccount extends AppCompatActivity {
             }
         });
     }
-    public void createNewAccount()
-    {
-        conn = connectionClass();
+    public void createNewAccount() {
         fNameID = (EditText)findViewById(R.id.firstNameInput);
         lNameID = (EditText)findViewById(R.id.lastNameInput);
         EmailID = (EditText)findViewById(R.id.emailInput);
@@ -66,80 +65,19 @@ public class NewAccount extends AppCompatActivity {
         UserNames= UserName.getText().toString();
         Password1 = password.getText().toString();
         Passwords = password2.getText().toString();
-        try {
-            if(Password1.equals(Passwords)) {
-                isAccount = testEmail();
-                isUserName = testUserName();
-                if (isAccount==false && isUserName==false ) {
-                    stmt = conn.createStatement();
-                    String sql = "INSERT INTO AccountTable " + "VALUES ('" + fname + "' , '" + lname + "' , '" + Emails + "' , '" + Passwords + "' , '" + UserNames + "')";
-                    stmt.executeUpdate(sql);
-                }
-            }
-        }
-        catch (Exception e)
+        boolean insertData = DatabaseHelp1.addData(fname,lname,Emails,Password1,UserNames);
+        if(insertData&&(Password1.equals(Passwords)))
         {
-            System.out.println(e.getMessage()) ;
+            toastMessage("Data entered right");
         }
-        Intent intent = new Intent(this, new_account2.class);
-        startActivity(intent);
+        else
+        {
+            toastMessage("Something went wrong");
+        }
     }
-    private boolean testUserName()
+    private void toastMessage(String message)
     {
-        conn = connectionClass();
-        try {
-            stmt = conn.createStatement();
-            String sql = "SELECT COUNT(Username) FROM AccountTable " + "WHERE Username = '"+ UserNames +"'";
-            ResultSet rs = stmt.executeQuery(sql);
-            rs.next();
-            int count = rs.getInt(1);
-            if(count==0)
-            {
-                return false;
-            }
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return true;
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    private boolean testEmail()
-    {
-        conn = connectionClass();
-        try {
-                stmt = conn.createStatement();
-                String sql = "SELECT COUNT(Email) FROM AccountTable " + "WHERE Email = '"+ Emails +"'";
-                ResultSet rs = stmt.executeQuery(sql);
-                rs.next();
-                int count = rs.getInt(1);
-                if(count==0)
-                {
-                    return false;
-                }
-            }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return true;
-    }
-    public Connection connectionClass()
-    {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection = null;
-        String ConnectionURL;
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://puppygram.database.windows.net:1433;DatabaseName=Puppygram;user=Noahkrill@puppygram;password=Uakron2019;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-            connection = DriverManager.getConnection(ConnectionURL);
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return connection;
-    }
 }
