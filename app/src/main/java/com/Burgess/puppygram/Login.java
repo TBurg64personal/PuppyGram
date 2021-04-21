e7787a3f94623702d7a32ea3995e38d610283f3f
TBurg64 <70659543+TBurg64@users.noreply.github.com>
2021-04-21 16:57:00 -0400

    Link the buttons together across all pages

--- app/src/main/java/com/Burgess/puppygram/Login.java
+++ app/src/main/java/com/Burgess/puppygram/Login.java
@@ -14,21 +14,36 @@ import android.widget.Button;
 
 public class Login extends AppCompatActivity {
     private Button loginButton;
+    private Button forgotButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_login);
         loginButton = (Button) findViewById(R.id.logIn2);
+        forgotButton = (Button) findViewById(R.id.forgotPass);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CheckAccount();
             }
         });
+        forgotButton.setOnClickListener(new View.OnClickListener()
+        {
+           @Override
+           public void onClick(View v)
+           {
+               goToPassword();
+           }
+        });
     }
     public void CheckAccount()
     {
         Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
+    }
+    public void goToPassword()
+    {
+        Intent intent = new Intent(this, forgot_password1.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/Burgess/puppygram/forgot_password2.java
+++ app/src/main/java/com/Burgess/puppygram/forgot_password2.java
@@ -2,13 +2,33 @@ package com.Burgess.puppygram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class forgot_password2 extends AppCompatActivity {
+
+    private Button GoBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password2);
+
+        GoBackButton = (Button) findViewById(R.id.backToLogin);
+
+        GoBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToLogin();
+            }
+        });
+    }
+    
+    public void backToLogin()
+    {
+        Intent intent = new Intent(this, Login.class );
+        startActivity(intent);
     }
 }
