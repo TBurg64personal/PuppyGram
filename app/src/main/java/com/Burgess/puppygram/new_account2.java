6c500320d36f3ed85a0e62d4bf0e05c86b6439b5
TBurg64 <70659543+TBurg64@users.noreply.github.com>
2021-04-24 18:34:01 -0400

    Fix up account creation page

--- app/src/main/java/com/Burgess/puppygram/new_account2.java
+++ app/src/main/java/com/Burgess/puppygram/new_account2.java
@@ -2,13 +2,28 @@ package com.Burgess.puppygram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class new_account2 extends AppCompatActivity {
-
+    private Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account2);
+        nextButton = (Button) findViewById(R.id.next1);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toAccount();
+            }
+        });
+    }
+    public void toAccount()
+    {
+        Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
     }
 }
