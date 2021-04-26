9b3de74a85596a7e7cd8f68a1d5311dc0dfa1db3
Connor Taylor <60106396+clt94@users.noreply.github.com>
2021-04-26 14:20:41 -0400

    Added navigation buttons to main feed

--- app/src/main/java/com/Burgess/puppygram/MainFeed.java
+++ app/src/main/java/com/Burgess/puppygram/MainFeed.java
@@ -3,6 +3,7 @@ package com.Burgess.puppygram;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +18,22 @@ public class MainFeed extends AppCompatActivity {
         GridView gridview = findViewById(R.id.gridview);
         gridview.setAdapter(new com.Burgess.puppygram.ImageFeedAdapter(this));
 
+        Button btn1 = (Button)findViewById(R.id.newPic);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainFeed.this, NewPicture.class));
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.profileButton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainFeed.this, ProfilePage.class));
+            }
+        });
     }
 }
